de84a4f4de64811fa0ce5bcc4b0a6b3037550c89
Steven Crutchfield <stevccru@icloud.com>
2021-05-09 18:27:24 -0400

    still playing with user input to append to webpage

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var requestURl = "https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=6e8d8d1bcb081e9149c96a12985c31c4"
+
 var currentDate = moment().format('MM/DD/YYYY');
 document.getElementById('currentDate').innerHTML = currentDate;
 
@@ -9,9 +11,17 @@ var button =  document.getElementById("runSearch")
 button.onclick = function (){
     var key = cityInput.value;
 
-    console.log(key)
+    
+    if (key, cityInput){
+        localStorage.setItem (key, cityInput);
+        location.reload()
+    }
 
 }
 
+for (let i=0; i <localStorage.length; i++){
+    var key =localStorage.key(i);
+    displayHistory.innerHTML +='${key}';
+}
 
 
